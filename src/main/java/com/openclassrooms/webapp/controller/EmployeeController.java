b86c5340b7ff7dda12ff51258b3813e46e74105b
JSigne <signe.josie@gmail.com>
2022-12-09 20:41:21 +0100

    [CONTROLLER] Ajout de méthodes pour créer un nouvel employé

--- src/main/java/com/openclassrooms/webapp/controller/EmployeeController.java
+++ src/main/java/com/openclassrooms/webapp/controller/EmployeeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -22,9 +24,22 @@ public class EmployeeController {
         return "home";
     }
 
+    @GetMapping("/createEmployee")
+    public String createEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "createEmployee";
+    }
+
     @GetMapping("/deleteEmployee/{id}")
     public ModelAndView deleteEmployee(@PathVariable("id") final int id) {
         employeeService.deleteEmployee(id);
+        return new ModelAndView("redirect:/");
+    }
+
+    @PostMapping("/saveEmployee")
+    public ModelAndView saveEmployee(@ModelAttribute Employee employee) {
+        employeeService.saveEmployee(employee);
         return new ModelAndView("redirect:/");
     }
 
