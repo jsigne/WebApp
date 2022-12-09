8c28b09304837a7fd9b8f3efc5dee300cc656af4
JSigne <signe.josie@gmail.com>
2022-12-09 17:41:59 +0100

    [REPOSITORY] Implementation de la communication avec l'API dans EmployeeProxy pour récupérer la liste des Employee

---
+++ src/main/java/com/openclassrooms/webapp/repository/EmployeeProxy.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.webapp.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.openclassrooms.webapp.model.Employee;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class EmployeeProxy {
+
+    @Autowired
+    private CustomProperties props;
+
+    /**
+     * Get all employees
+     * @return An iterable of all employees
+     */
+
+    public Iterable<Employee> getEmployees() {
+        String baseApiUrl = props.getApiUrl();
+        String getEmployeesUrl = baseApiUrl + "/employees";
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Iterable<Employee>> response = restTemplate.exchange(
+                getEmployeesUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Iterable<Employee>>() {}
+        );
+
+        log.debug("Get Employees call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+}
